8b60ef7f70a8316cbcd23c9b352d71c46db3b573
edi2xr <mutuaadrian73@gmail.com>
2025-06-28 16:09:48 +0300

    documents

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
   let totalIncome = 0;
   let totalExpenses = 0
+  const incomeInput = document.getElementById("income-input");
+  const setIncomeBtn = document.getElementById("set-income-btn");
+  const expenseNameInput = document.getElementById("expense-name");
+  const expenseAmountInput = document.getElementById("expense-amount");
+  const addExpenseBtn = document.getElementById("add-expense-btn");
