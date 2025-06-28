69e3ce0d2b511d8d45b4bb0ccbd19619e2cce5b4
edi2xr <mutuaadrian73@gmail.com>
2025-06-28 16:11:17 +0300

    displays

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
   let totalIncome = 0;
   let totalExpenses = 0
+
   const incomeInput = document.getElementById("income-input");
   const setIncomeBtn = document.getElementById("set-income-btn");
   const expenseNameInput = document.getElementById("expense-name");
   const expenseAmountInput = document.getElementById("expense-amount");
   const addExpenseBtn = document.getElementById("add-expense-btn");
+
+  onst totalIncomeDisplay = document.getElementById("total-income");
+  const totalExpensesDisplay = document.getElementById("total-expenses");
+  const balanceDisplay = document.getElementById("balance");
+  const expenseList = document.getElementById("expense-list");
